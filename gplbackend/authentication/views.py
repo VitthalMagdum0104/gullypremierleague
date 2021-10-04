c671ea890a54f76a1dfaf896faafb41462ce0589
Pooja jain <poojajain1@yahoo.com>
2021-10-04 20:31:46 +0530

    facebook login

--- gplbackend/authentication/views.py
+++ gplbackend/authentication/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
+# from django.contrib.auth import logout as auth_logout
+# from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 
 def login(request):
     return render(request, 'login.html')
+
+# @login_required(login_url='/')
+# def home(request):
+#     return render_to_response('home.html')
+
+
+# def logout(request):
+#     auth_logout(request)
+#     return redirect('/')
